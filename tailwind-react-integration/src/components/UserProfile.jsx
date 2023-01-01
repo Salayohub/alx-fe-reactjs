05db8f4e702f2dafec41372e942e006b4ac86e5b
salahudeen abdulkareem <salayo4real@gmail.com>
2023-01-01 02:30:56 +0100

    responsive design added

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,10 +2,13 @@ import profile from "../assets/profile.png"
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto,my-20 rounded-lg shadow-lg">
-      <img src={profile} alt="User" className="rounded-full w-36 h-36 mx-auto"/>
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="user-profile bg-gray-100 
+    p-4 md:p-8 lg:p-8
+    max-w-xs md:max-w-sm lg:max-w-sm 
+    mx-auto,my-20 rounded-lg shadow-lg">
+      <img src={profile} alt="User" className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"/>
+      <h1 className="text-lg md:text-xl lg:text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-sm md:text-base  text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }

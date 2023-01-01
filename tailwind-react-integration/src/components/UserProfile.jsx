bcff50a45c112f60d142449eb5b04cdf628700d4
salahudeen abdulkareem <salayo4real@gmail.com>
2023-01-01 03:07:22 +0100

    hover and shadow styling added

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,9 +5,9 @@ function UserProfile() {
     <div className="user-profile bg-gray-100 
     sm:p-4 md:p-8 lg:p-8
     max-w-xs md:max-w-sm lg:max-w-sm 
-    mx-auto,my-20 rounded-lg shadow-lg">
-      <img src={profile} alt="User" className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"/>
-      <h1 className="text-lg md:text-xl lg:text-xl text-blue-800 my-4">John Doe</h1>
+    mx-auto,my-20 rounded-lg shadow-lg hover:shadow-xl">
+      <img src={profile} alt="User" className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto  transition-transform duration-300 ease-in-out hover:scale-110"/>
+      <h1 className="text-lg md:text-xl lg:text-xl text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
       <p className="text-sm md:text-base  text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );

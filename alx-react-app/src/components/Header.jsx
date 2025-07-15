4b00152e3fc083905a3f76956c8ee119d430b97c
salahudeen abdulkareem <salayo4real@gmail.com>
2025-07-15 05:49:07 +0100

    new update

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,8 +1,16 @@
 function Header() {
+  const headerstyle = {
+    marginTop: '25px',
+    color: 'red',
+    fontSize: '25px',
+    border: '2px dotted yellow',
+    borderRadius: '10px'
+  }
 return(
-<header>
+<header style={headerstyle}>
   <h1>My Favorite Cities</h1>
 </header>
+
 );
 }
 
--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
 return( 
-<main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
+<main style={{backgroundColor: 'white',color: 'black', padding:'5px 0px', margin: '10px'}}>
+  <p style={{fontSize:'20px',fontFamily:'roboto',fontWeight:'bold'}}>I love to visit New York, Paris, and Tokyo.</p>
 </main>
 );
 }

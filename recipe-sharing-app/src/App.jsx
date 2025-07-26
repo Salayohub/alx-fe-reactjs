86d487988fa7b8808c3cd7ebf023cd6681005a48
salahudeen abdulkareem <salayo4real@gmail.com>
2025-07-26 09:46:14 +0100

    new update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,25 @@
+// App.jsx
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import RecipeList from './components/RecipeList'
-import AddRecipeForm from './components/EditRecipeForm'
-import './App.css'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeDetails from './components/RecipeDetails'
 
 function App() {
- 
-
   return (
-    <>
-      <p>hello welcome get your recipes</p>
-      <RecipeList />
-      <AddRecipeForm />
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <p>hello welcome get your recipes</p>
+            <RecipeList />
+            <AddRecipeForm />
+          </>
+        }
+      />
+      <Route path="/recipe/:id" element={<RecipeDetails />} />
+    </Routes>
   )
 }
 
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,17 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import RecipeDetails from './components/RecipeDetails'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )

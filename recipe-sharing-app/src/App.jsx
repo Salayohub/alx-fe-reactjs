1603efa919bee96cec7e216b34d7bdd1beeb956d
salahudeen abdulkareem <salayo4real@gmail.com>
2025-07-26 09:03:22 +0100

    new add

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <p>hello welcome get your recipes</p>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  )
+}
+
+export default App

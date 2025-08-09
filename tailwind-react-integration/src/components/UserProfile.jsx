670c000e2ea4c98b92e2a0195eae9beda1594073
salahudeen abdulkareem <salayo4real@gmail.com>
2025-08-09 21:24:14 +0100

    updated

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import profile from "../assets/profile.png"
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto,my-20 round-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto,my-20 rounded-lg shadow-lg">
       <img src={profile} alt="User" className="rounded-full w-36 h-36 mx-auto"/>
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>

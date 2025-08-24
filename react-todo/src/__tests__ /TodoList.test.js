43dec2c9e4790eb51b3d5d17c42f311a80489369
salahudeen abdulkareem <salayo4real@gmail.com>
2025-08-24 22:31:26 +0100

    file added

---
+++ react-todo/src/__tests__ /TodoList.test.js
@@ -0,0 +1,30 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import TodoList from './TodoList'
+
+test('renders initial todos', () => {
+  render(<TodoList />)
+  expect(screen.getByText('Learn React')).toBeInTheDocument()
+  expect(screen.getByText('Build a Todo App')).toBeInTheDocument()
+})
+
+test('adds a new todo', () => {
+  render(<TodoList />)
+  fireEvent.change(screen.getByPlaceholderText(/add new todo/i), {
+    target: { value: 'Write tests' },
+  })
+  fireEvent.click(screen.getByText(/add todo/i))
+  expect(screen.getByText('Write tests')).toBeInTheDocument()
+})
+
+test('toggles a todo', () => {
+  render(<TodoList />)
+  const todo = screen.getByText('Learn React')
+  fireEvent.click(todo)
+  expect(todo).toHaveClass('completed')
+})
+
+test('deletes a todo', () => {
+  render(<TodoList />)
+  fireEvent.click(screen.getAllByText(/delete/i)[0])
+  expect(screen.queryByText('Learn React')).not.toBeInTheDocument()
+})
